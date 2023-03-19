defa232258b66bf927c0995da3e943e5021b2f0e
Sinan Dogan <sinnandogann@outlook.com>
2023-03-19 23:27:35 +0300

    first commit

---
+++ search-meals/src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+
+
+<!-- search input  -->
+<div class="flex flex-col p-8">
+  
+    <input type="text" class="rounded border-2 border-gray-200 w-full" placeholder="Search for Meals">
+ 
+
+
+<div class="flex gap-2 justify-center mt-2">
+<router-link :to="{name: 'byLetter', params:{letter}}" v-for="letter of letters" :key="letter">
+{{ letter }}
+</router-link>
+</div>
+</div>
+
+<pre>{{ ingredients }}</pre>
+</template>
+
+
+<script setup>
+import {computed,onMounted,ref} from 'vue'
+import store from '../store';
+import axiosClient from '../axiosClient.js'
+
+const letters='ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
+const ingredients = ref([])
+
+onMounted(async()=>{
+  const response =await axiosClient.get('/list.php?i=list')
+    console.log(response.data)
+    ingredients.value=response.data
+})
+</script>
---
+++ search-meals/src/views/MealList.vue
@@ -0,0 +1,13 @@
+<template>
+
+<div>
+    Meal List
+</div>
+
+</template>
+
+
+
+<script>
+
+</script>
